0803e84c19abed9415af72d2ba7848e1b9853588
Idowu Sunday <isunday5@byupathway.edu>
2025-09-23 18:39:10 +0100

    Modular javascript with ES modules

---
+++ Scripts/sections.mjs
@@ -0,0 +1,13 @@
+export function populateSelections(sections) {
+  const sectionSelect = document.querySelector("#sectionNumber");
+  byuiCourse.sections.forEach((section) => {
+    const option = document.createElement("option");
+    option.value = section.sectionNumber;
+    option.textContent = `${section.sectionNumber}`;
+    sectionSelect.appendChild(option);
+  });
+  
+}
+
+
+
